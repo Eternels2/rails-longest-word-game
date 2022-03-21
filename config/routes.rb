74515c43e21dba3a549cb76d07c7e00b4b4732a0
eric zarazir <eric.zarazir@gmail.com>
2022-03-21 12:19:42 +0100

    rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'articles#index'
+  get '/new', to: 'games#new'
+  post '/score', to: 'games#score'
+  get '/result', to: 'games#result'
+  get '/show', to: 'games#show'
 end
